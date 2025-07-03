c6fc0c4f3ba207bcc60c8dee7ec49043ec83cfb2
lzhang-etsy <lzhang@etsy.com>
2025-07-03 12:23:11 -0400

    update the pytest

---
+++ .github/actions/pytest-coverage-action/scripts/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Install dependencies
+cd "$WORK_DIR"
+python -m pip install --upgrade pip
+
+if [[ -n "$WORKLOAD_IDENTITY_PROVIDER" && -n "$SERVICE_ACCOUNT" ]]; then
+    pip install keyring && pip install keyrings.google-artifactregistry-auth
+fi
+
+pip install -r requirements.txt
+pip install pytest pytest-cov
+
+# Build and Run Unit test
+pytest  \
+  --cov="$PACKAGE_NAME" \
+  --cov-report=term \
+  --cov-config=.coveragerc \
+  --cov-fail-under="$COVERAGE_THRESHOLD" \
+  -q | sed 's|src/||g' > coverage.txt
+
+# Extract summary
+echo "<!-- pytest-report for $PACKAGE_NAME -->" > summary.txt
+COVERAGE=$(awk '/^TOTAL/ {print $(NF)}' coverage.txt | sed 's/%//')
+if (( $(echo "$COVERAGE >= $COVERAGE_THRESHOLD" | bc -l) )); then
+  echo "### ✅ Pytest & Coverage Report for $PACKAGE_NAME" >> summary.txt
+else
+  echo "### ❌ Pytest & Coverage Report for $PACKAGE_NAME" >> summary.txt
+fi
+echo '```' >> summary.txt
+awk '/^Name.*Stmts.*Miss.*Cover/ {start=1} start' coverage.txt >> summary.txt
+echo '```' >> summary.txt
