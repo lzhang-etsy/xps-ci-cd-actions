5f93572f55c550da94076c43459ccc9dbef54f12
lzhang-etsy <lzhang@etsy.com>
2025-07-04 12:07:11 -0400

    install from gar

--- .github/actions/pytest-coverage-action/scripts/run.sh
+++ .github/actions/pytest-coverage-action/scripts/run.sh
@@ -4,19 +4,28 @@ set -e
 set -o pipefail
 
 # Install dependencies
+cd "$WORK_DIR"
 python -m pip install --upgrade pip
 
+# Check for auth variables and install dependencies accordingly
 if [[ -n "$WORKLOAD_IDENTITY_PROVIDER" && -n "$SERVICE_ACCOUNT" ]]; then
-  python -m pip install keyring  keyrings.google-artifactregistry-auth
+  echo "Auth variables detected. Installing with Artifact Registry."
+  python -m pip install keyring keyrings.google-artifactregistry-auth
+  python -m pip install --extra-index-url https://us-central1-python.pkg.dev/etsy-xscitools-dev/experimentation-pypi/simple -r requirements.txt
+else
+  echo "No auth variables detected. Installing from standard index."
+  python -m pip install -r requirements.txt
 fi
 
-python -m pip install -r requirements.txt
-python -m pip install pytest pytest-cov pytest-pythonpath
+python -m pip install pytest pytest-cov
 
-# Build and Run Unit tests
-
-#pytest
-pytest --cov-report=term --cov-config=.coveragerc --cov-fail-under="$COVERAGE_THRESHOLD"  -vv --show-capture=all  -o log_cli=true -o log_cli_level=DEBUG | sed 's|src/||g' | tee coverage.txt
+# Build and Run Unit test
+pytest -v -rA \
+  --cov="$PACKAGE_NAME" \
+  --cov-report=term \
+  --cov-config=.coveragerc \
+  --cov-fail-under="$COVERAGE_THRESHOLD" \
+  -q | sed 's|src/||g' > coverage.txt
 
 # Extract summary
 echo "<!-- pytest-report for $PACKAGE_NAME -->" > summary.txt
